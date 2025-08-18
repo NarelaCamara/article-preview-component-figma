7275d6eb4cb9f6f914056b96fd1583c76455696a
RoxanaCamara <roxana.camara07@gmail.com>
2025-08-18 20:54:43 -0300

    feat: tooltip

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import facebook from "../../assets/icon-facebook.svg";
 import pinterest from "../../assets/icon-pinterest.svg";
 import twitter from "../../assets/icon-twitter.svg";
+import { Tooltip } from "../tooltip/Tooltip";
 
 export const Profile = ({
   profileImage,
@@ -62,38 +63,11 @@ export const Profile = ({
             </div>
           </>
 
-          <div className="col-start-5">
-            {/**Desktop y tablet */}
-            <div
-              className={` px-[38px] py-[16px]  bg-[#48556A] absolute top-[50vh] right-[22vw]   rounded-xl hidden ${
-                share ? " md:block " : "  "
-              }  min-w-[248px]`}
-            >
-              <div className="grid grid-cols-5 grid-rows-1 gap-1 place-items-center ">
-                <h4 className={`text-[#9DAEC2] text-sm font-[Manrope]`}>
-                  SHARE
-                </h4>
-                <div className="col-span-3">
-                  <button className="rounded-full ml-4  h-8">
-                    <img src={facebook} className="w-5 h-5" alt="facebook" />
-                  </button>
-                  <button className="rounded-full ml-4  h-8">
-                    <img src={twitter} className="w-5 h-5" alt="twiter" />
-                  </button>
-                  <button className="rounded-full ml-4  h-8">
-                    <img src={pinterest} className="w-5 h-5" alt="pinterest" />
-                  </button>
-                </div>
-              </div>
-
-              <div
-                className=" absolute top-[8.2vh] right-[9.8vw] w-0 h-0 
-    border-l-[12px] border-l-transparent 
-    border-r-[12px] border-r-transparent 
-    border-t-[12px] border-t-[#0546af]"
-              ></div>
+          <div className="col-start-5 relative">
+            <div className="absolute z-50 -top-22 -left-20 ">
+              <Tooltip share={share} />
             </div>
-            {/**Desktop y tablet */}
+
             <button
               className={` ${
                 share ? "bg-[#6E8098]" : "bg-white "
---
+++ src/components/tooltip/Tooltip.tsx
@@ -0,0 +1,34 @@
+import facebook from "../../assets/icon-facebook.svg";
+import pinterest from "../../assets/icon-pinterest.svg";
+import twitter from "../../assets/icon-twitter.svg";
+
+export const Tooltip = ({ share }: { share: boolean }) => {
+  return (
+    <div className={`hidden ${share ? " md:block " : "  "}`}>
+      <div
+        className={`px-[38px] py-[16px]  bg-[#48556A] rounded-xl min-w-[248px]`}
+      >
+        <div className="grid grid-cols-4 grid-rows-1 gap-1  place-items-center ">
+          <h4 className={`text-[#9DAEC2] text-sm font-[Manrope]`}>SHARE</h4>
+          <div className="col-span-3">
+            <button className="rounded-full ml-4  h-8">
+              <img src={facebook} className="w-5 h-5" alt="facebook" />
+            </button>
+            <button className="rounded-full ml-4  h-8">
+              <img src={twitter} className="w-5 h-5" alt="twiter" />
+            </button>
+            <button className="rounded-full ml-4  h-8">
+              <img src={pinterest} className="w-5 h-5" alt="pinterest" />
+            </button>
+          </div>
+        </div>
+      </div>
+      <div
+        className="absolute top-[100%] right-[56%] w-0 h-0 
+    border-l-[12px] border-l-transparent 
+    border-r-[12px] border-r-transparent 
+    border-t-[12px] border-t-[#48556A]"
+      ></div>
+    </div>
+  );
+};
